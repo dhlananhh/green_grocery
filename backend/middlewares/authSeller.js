a0b5197c5a134d852234fb56af8aad14f03a3972
Lan Anh <dhlananh2309@gmail.com>
2025-04-27 20:15:19 +0700

    feat: create a new middleware configuration folder

---
+++ backend/middlewares/authSeller.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken"
+
+const authSeller = async (req, res, next) => {
+  const { sellerToken } = req.cookies
+
+  if (!sellerToken) {
+    return res.json({ success: false, message: "Not Authorized" })
+  }
+}
+
+
+export default authSeller
