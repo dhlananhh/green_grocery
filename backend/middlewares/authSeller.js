529ec5a404673f1afea3354af715ee98fa6a40cf
Lan Anh <dhlananh2309@gmail.com>
2025-04-28 13:17:25 +0700

    feat: create a seller authentication configuration

--- backend/middlewares/authSeller.js
+++ backend/middlewares/authSeller.js
@@ -1,11 +1,24 @@
 import jwt from "jsonwebtoken"
 
+
+// auth seller
 const authSeller = async (req, res, next) => {
   const { sellerToken } = req.cookies
 
   if (!sellerToken) {
     return res.json({ success: false, message: "Not Authorized" })
   }
+
+  try {
+      const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET)
+      if (tokenDecode.email === process.env.SELLER_EMAIL) {
+        next()
+      } else {
+        return res.json({ success: false, message: "Not Authorized" })
+      }
+    } catch (error) {
+      return res.json({ success: false, message: error.message })
+    }
 }
 
 
