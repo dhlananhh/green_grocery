985b447d2f9853e2dd38082c4ed3e18dd3f2dbfa
Lan Anh <dhlananh2309@gmail.com>
2025-05-10 01:37:44 +0700

    chore: update App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Routes, Route } from "react-router-dom"
+// import Home from "../Home"
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
+      {/* <div>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+        </Routes>
+      </div> */}
     </div>
   )
 }
