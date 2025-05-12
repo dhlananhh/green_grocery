2d2a664e9d5eaf722e05aea8f69ff8848e53a05c
Lan Anh <dhlananh2309@gmail.com>
2025-05-12 11:33:11 +0700

    feat: add routing for product pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,20 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { Toaster } from "react-hot-toast"
 import Footer from "./components/Footer.jsx";
+import { useAppContext } from "./context/AppContext.jsx";
+import LogIn from "./components/LogIn.jsx";
+import AllProducts from "./pages/AllProducts.jsx";
+import ProductCategory from "./pages/ProductCategory.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const { showUserLogin } = useAppContext()
 
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <LogIn /> : null}
 
       <Toaster />
 
@@ -21,6 +28,9 @@ const App = () => {
       >
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:category" element={<ProductCategory />} />
+          <Route path="/products/:category/:id" element={<ProductDetails />} />
         </Routes>
       </div>
 
