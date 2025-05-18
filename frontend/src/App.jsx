1525461fa9b6e7e13f2fc5c2511713aef7e25ba1
Lan Anh <dhlananh2309@gmail.com>
2025-05-18 13:57:37 +0700

    feat: add seller login route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import React from "react"
+import Navbar from "./components/Navbar"
+import { Routes, Route, useLocation } from "react-router-dom"
+import Home from "./pages/Home.jsx"
 import { Toaster } from "react-hot-toast"
-import Footer from "./components/Footer.jsx";
-import { useAppContext } from "./context/AppContext.jsx";
-import LogIn from "./components/LogIn.jsx";
-import AllProducts from "./pages/AllProducts.jsx";
-import ProductCategory from "./pages/ProductCategory.jsx";
-import ProductDetails from "./pages/ProductDetails.jsx";
-import Cart from "./pages/Cart.jsx";
-import AddAddress from "./pages/AddAddress.jsx";
-import MyOrders from "./pages/MyOrders.jsx";
+import Footer from "./components/Footer.jsx"
+import { useAppContext } from "./context/AppContext.jsx"
+import LogIn from "./components/LogIn.jsx"
+import AllProducts from "./pages/AllProducts.jsx"
+import ProductCategory from "./pages/ProductCategory.jsx"
+import ProductDetails from "./pages/ProductDetails.jsx"
+import Cart from "./pages/Cart.jsx"
+import AddAddress from "./pages/AddAddress.jsx"
+import MyOrders from "./pages/MyOrders.jsx"
+import SellerLogin from "./components/seller/SellerLogin.jsx"
 
 const App = () => {
-  const isSellerPath = useLocation().pathname.includes("seller");
-  const { showUserLogin } = useAppContext()
+  const isSellerPath = useLocation().pathname.includes("seller")
+  const { showUserLogin, isSeller } = useAppContext()
 
   return (
     <div>
@@ -37,12 +38,14 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
+
+          <Route path="/seller" element={isSeller ? null : <SellerLogin />} />
         </Routes>
       </div>
 
       {!isSellerPath && <Footer />}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
